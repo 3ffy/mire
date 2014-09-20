48e4197c637f157d7a18d6cdf5b996ddb251bf13
Aurelien GY <aureliengy@gmail.com>
2014-09-20 10:00:51 +0200

    color convertions

--- mire.js
+++ mire.js
@@ -36,39 +36,101 @@ mire._getVerboseRGBRGBA = function(red, green, blue, alpha, simplifyToRGB, useEx
     return ret;
 };
 
-mire.color = function(opacity){
+mire._HUEToRGB = function(p, q, t) {
+    if (t < 0) t += 1;
+    if (t > 1) t -= 1;
+    if (t < 1 / 6) return p + (q - p) * 6 * t;
+    if (t < 1 / 2) return q;
+    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+    return p;
+};
+
+mire.color = function(opacity) {
+    this._hex = undefined;
     this._name = undefined;
     this._lambda = undefined;
     this._pantone = undefined;
     this._rgb = undefined;
     this._hsl = undefined;
+    this._hsb = undefined;
     this._cmyk = undefined;
-    this._alpha = (typeof opacity == 'undefined')? 1 : opacity;
+    this._alpha = (typeof opacity == 'undefined') ? 1 : opacity;
 };
 
-mire.color.prototype.getDeterminist = function(){
-    if(this._rgb === undefined){
+mire.color.prototype.getHEX = function() {
+    if (this._hex === undefined) {
+        this._toHEX();
+    }
+    return new mire.colorHEX(this._hex);
+};
+
+mire.color.prototype.getRGB = function() {
+    if (this._rgb === undefined) {
+        this._toRGB();
+    }
+    return new mire.colorRGB(this._rgb.r, this._rgb.g, this._rgb.g);
+};
+
+mire.color.prototype.getRGBA = function(opacity) {
+    if (this._rgb === undefined) {
+        this._toRGB();
+    }
+    if (typeof opacity != number) {
+        opacity = (this._alpha === undefined) ? 1 : this._alpha;
+    }
+    return new mire.colorRGB(this._rgb.r, this._rgb.g, this._rgb.b, opacity);
+};
+
+mire.color.prototype.getHSL = function() {
+    if (this._hsl === undefined) {
+        this._toHSL();
+    }
+    return new mire.colorHSL(this._hsl.h, this._hsl.s, this._hsl.l);
+};
+
+mire.color.prototype.getHSLA = function(opacity) {
+    if (this._hsl === undefined) {
+        this._toHSL();
+    }
+    if (typeof opacity != number) {
+        opacity = (this._alpha === undefined) ? 1 : this._alpha;
+    }
+    return new mire.colorHSLA(this._hsl.h, this._hsl.s, this._hsl.l, opacity);
+};
+
+mire.color.prototype.getHSB = function() {
+    if (this._hsb === undefined) {
+        this._toHSB();
+    }
+    return new mire.colorHSB(this._hsb.h, this._hsb.s, this._hsb.b);
+};
+
+mire.colorRGBA.prototype.toHEX = function() {
+    if (this._rgb === undefined) {
         this._toRGB();
     }
     //convert to hexadecimal representation
-    var alpha = (this._alpha*100).toString(16);
-    var red = this._rgb.r.toString(16);
-    var green = this._rgb.g.toString(16);
-    var blue = this._rgb.b.toString(16);
+    var red = '' + this._rgb.r.toString(16);
+    var green = '' + this._rgb.g.toString(16);
+    var blue = '' + this._rgb.b.toString(16);
     //get results with 2 digits (except if allowCombine == true and result combinable on 3 digit instead of 6)
-    if(alpha.length < 2){
+    red = red.padLeft(2, '0');
+    green = green.padLeft(2, '0');
+    blue = blue.padLeft(2, '0');
+    this._hex = '' + red + green + blue;
+};
+
+mire.color.prototype.getDeterminist = function() {
+    if (this._hex === undefined) {
+        this._toHEX();
+    }
+    //convert to hexadecimal representation (with 2 digits)
+    var alpha = (this._alpha * 100)
+        .toString(16);
+    if (alpha.length < 2) {
         alpha = alpha.padLeft(2, '0');
-    }    
-    if(red.length < 2){
-        red = red.padLeft(2, '0');
     }
-    if(green.length < 2){
-        green = green.padLeft(2, '0');
-    }
-    if(blue.length < 2){
-        blue = blue.padLeft(2, '0');
-    }
-    return '' + alpha + red + green + blue;
+    return '' + alpha + this._hex;
 };
 
 mire.colorRGBA = function(red, green, blue, opacity) {
@@ -86,6 +148,74 @@ mire.colorRGBA.prototype.getVerbose = function(simplifyToRGB, useExtraSpaces, us
     return mire._getVerboseRGBRGBA(this._rgb.r, this._rgb.g, this._rgb.b, this._alpha, simplifyToRGB, useExtraSpaces, usePercentage);
 };
 
+mire.colorRGBA.prototype.toHSL = function() {
+    //normalize the values
+    var r = this._rgb.r / 255;
+    var g = this._rgb.g / 255;
+    var b = this._rgb.b / 255;
+    //get m & M a initial values
+    var max = Math.max(r, g, b);
+    var min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+    //detect if achromatic
+    if (max == min) {
+        h = s = 0;
+    } else {
+        var d = max - min;
+        s = (l > 0.5 ? ) d / (2 - max - min): d / (max + min);
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
+        }
+        h /= 6;
+    }
+    this._hsl = {};
+    this._hsl.h = h;
+    this._hsl.s = s;
+    this._hsl.l = l;
+};
+
+mire.colorRGBA.prototype.toHSB = function() {
+    //normalize the values
+    var r = this._rgb.r / 255;
+    var g = this._rgb.g / 255;
+    var b = this._rgb.b / 255;
+    //get m & M a initial values
+    var max = Math.max(r, g, b);
+    var min = Math.min(r, g, b);
+    var h, s, v = max;
+    var d = max - min;
+    s = max == 0 ? 0 : d / max;
+    //detect if achromatic
+    if (max == min) {
+        h = 0;
+    } else {
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
+        }
+        h /= 6;
+    }
+    this._hsb = {};
+    this._hsb.h = h;
+    this._hsb.s = s;
+    this._hsb.b = v;
+};
+
 mire.colorRGB = function(red, green, blue) {
     mire.color.call(this);
     this._rgb = {};
@@ -94,12 +224,8 @@ mire.colorRGB = function(red, green, blue) {
     this._rgb.b = blue;
 };
 
-mire.colorRGB.prototype = new mire.colorRGBA(0,0,0,0);
+mire.colorRGB.prototype = new mire.colorRGBA(0, 0, 0, 0);
 mire.colorRGB.prototype.constructor = mire.colorRGB;
-
-mire.colorRGB.prototype.toRGBA = function(opacity) {
-    return new colorRGBA(this._rgb.r, this._rgb.g, this._rgb.b, opacity);
-};
 
 mire.colorRGB.prototype.getVerbose = function(useExtraSpaces, usePercentage) {
     return mire._getVerboseRGBRGBA(this._rgb.r, this._rgb.g, this._rgb.b, this._alpha, true, useExtraSpaces, usePercentage);
@@ -123,16 +249,19 @@ mire.colorHEX = function(hex) {
             red = parseInt('' + hex[0] + hex[0], 16);
             green = parseInt('' + hex[1] + hex[1], 16);
             blue = parseInt('' + hex[2] + hex[2], 16);
+            this._hex = '' + hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
         } else if (hexSize == 6) {
             red = parseInt('' + hex[0] + hex[1], 16);
             green = parseInt('' + hex[2] + hex[3], 16);
             blue = parseInt('' + hex[4] + hex[5], 16);
+            this._hex = '' + hex[0] + hex[1] + hex[2] + hex[3] + hex[4] + hex[5];
         }
         hex = '' + parseInt(hex, 16);
     } else {
         red = (hex >> 16) & 0xff;
         green = (hex >> 8) & 0xff;
         blue = hex & 0xff;
+        this._hex = '' + hex.toString(16);
     }
     //if the value was not convertible to rgb uplets, throw an exception
     if (red === undefined || green === undefined || blue === undefined) {
@@ -144,7 +273,7 @@ mire.colorHEX = function(hex) {
     this._rgb.b = blue;
 };
 
-mire.colorHEX.prototype = new mire.color();
+mire.colorHEX.prototype = new mire.colorRGB();
 mire.colorHEX.prototype.constructor = mire.colorHEX;
 
 mire.colorHEX.prototype.getVerbose = function(upperCase, allowCombine) {
@@ -182,17 +311,6 @@ mire.colorName = function(name) {
 mire.colorName.prototype = new mire.colorHEX(0);
 mire.colorName.prototype.constructor = mire.colorName;
 
-mire.colorHSL = function(hue, saturation, lightness) {
-    mire.color.call(this);
-    this._hsl = {};
-    this._hsl.h = hue;
-    this._hsl.s = saturation;
-    this._hsl.l = lightness;
-};
-
-mire.colorHSL.prototype = new mire.color();
-mire.colorHSL.prototype.constructor = mire.colorHSL;
-
 mire.colorHSLA = function(hue, saturation, lightness, opacity) {
     mire.color.call(this, opacity);
     this._hsl = {};
@@ -204,11 +322,93 @@ mire.colorHSLA = function(hue, saturation, lightness, opacity) {
 mire.colorHSLA.prototype = new mire.color();
 mire.colorHSLA.prototype.constructor = mire.colorHSLA;
 
-// mire.colorHSV = function(hue, saturation, value) {
-//     this.hue = hue;
-//     this.saturation = saturation;
-//     this.value = value;
-// };
+mire.colorHSLA.prototype._toRGB = function() {
+    var r, g, b;
+    //detect if achromatic
+    if (this._hsl.s == 0) {
+        r = g = b = this._hsl.l;
+    } else {
+        var q = (this._hsl.l < 0.5) ? this._hsl.l * (1 + this._hsl.s) : this._hsl.l + this._hsl.s - this._hsl.l * this._hsl.s;
+        var p = 2 * this._hsl.l - q;
+        r = mire._HUEToRGB(p, q, this._hsl.h + 1 / 3);
+        g = mire._HUEToRGB(p, q, this._hsl.h);
+        b = mire._HUEToRGB(p, q, this._hsl.h - 1 / 3);
+    }
+    this._rgb = {};
+    this._rgb.r = Math.round(r * 255);
+    this._rgb.g = Math.round(g * 255);
+    this._rgb.b = Math.round(b * 255);
+};
+
+mire.colorHSL.prototype.toHSB = function(){
+    //TODO
+};
+
+mire.colorHSL = function(hue, saturation, lightness) {
+    mire.color.call(this);
+    this._hsl = {};
+    this._hsl.h = hue;
+    this._hsl.s = saturation;
+    this._hsl.l = lightness;
+};
+
+mire.colorHSL.prototype = new mire.colorHSLA();
+mire.colorHSL.prototype.constructor = mire.colorHSL;
+
+mire.colorHSL.prototype.getVerbose = function() {
+    //TODO passer la valeur en angles + pourcentages pour sortir la notation css != des calculs
+};
+
+// = HSV
+mire.colorHSB = function(hue, saturation, brightness) {
+    mire.color.call(this);
+    this._hsl = {};
+    this._hsl.h = hue;
+    this._hsl.s = saturation;
+    this._hsl.l = lightness;
+};
+
+mire.colorHSB.prototype._toRGB = function() {
+    var r, g, b;
+    var i = Math.floor(this._hsb.h * 6);
+    var f = this._hsb.h * 6 - i;
+    var p = this._hsb.h.b * (1 - this._hsb.s);
+    var q = this._hsb.b * (1 - f * this._hsb.s);
+    var t = this._hsb.b * (1 - (1 - f) * this._hsb.s);
+    switch (i % 6) {
+        case 0:
+            r = this._hsb.b;
+            g = t;
+             b = p;
+            break;
+        case 1:
+            r = q; g = this._hsb.b; b = p;
+            break;
+        case 2:
+            r = p; g = this._hsb.b; b = t;
+            break;
+        case 3:
+            r = p; g = q; b = this._hsb.b;
+            break;
+        case 4:
+            r = t; g = p; b = this._hsb.b;
+            break;
+        case 5:
+            r = this._hsb.b; g = p; b = q;
+            break;
+    }
+    this._rgb = {};
+    this._rgb.r = r;
+    this._rgb.g = g;
+    this._rgb.b = b;
+};
+
+mire.colorHSB.prototype._toHSL = function() {
+    //TOOD
+};
+
+mire.colorHSB.prototype = new mire.color();
+mire.colorHSB.prototype.constructor = mire.colorHSB;
 
 /**
  * [colorCMYK description]
@@ -229,7 +429,6 @@ mire.colorCMYK = function(cyan, magenta, yellow, key) {
 
 mire.colorCMYK.prototype = new mire.color();
 mire.colorCMYK.prototype.constructor = mire.colorCMYK;
-
 
 // mire.colorLambda = function(lambda) {
 //     this.lambda = lambda;
